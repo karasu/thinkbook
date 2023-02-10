17581a9baaa582f1fc5cc589585d5411843fd8d1
Gustau Castells <gustaucastells@gmail.com>
2023-02-10 12:04:15 +0100

    Add font

--- myarch.sh
+++ myarch.sh
@@ -1,6 +1,15 @@
-# Vars
+# Arch install script with my favourites
 
-set_config_vars() {
+aur() {
+  echo "Installing $1 from AUR..."
+  git clone https://aur.archlinux.org/$1.git
+  cd $1
+  makepkg -s
+  ${INSTALL_ZST} *.zst
+  cd ..
+}
+
+set_vars() {
   USERNAME="karasu"
   CONF_DIR="/home/${USERNAME}/.config"
 
@@ -21,204 +30,227 @@ set_config_vars() {
   PROGRAMARI_USERNAME="compartit"
   PROGRAMARI_PASSWORD=""
 
-# Prepare disk
+  export EDITOR=nano
+}
 
-${PARTED} ${INSTALL_DEV} -- mklabel gpt \
+prepare_disk() {
+  ${PARTED} ${INSTALL_DEV} -- mklabel gpt \
     mkpart myboot fat32 1MiB 512MiB \
     mkpart myroot btrfs 512MiB -1s
 
-# Create filesystems
-mkfs.vfat ${BOOT_PART}
-mkfs.btrfs ${ROOT_PART}
+  # Create filesystems
+  mkfs.vfat ${BOOT_PART}
+  mkfs.btrfs ${ROOT_PART}
 
-# Mount install destination
-mkdir /mnt
-mount ${ROOT_PART} /mnt
+  # Mount install destination
+  mkdir /mnt
+  mount ${ROOT_PART} /mnt
 
-mkdir /mnt/boot
-mount ${BOOT_PART} /mnt/boot
+  mkdir /mnt/boot
+  mount ${BOOT_PART} /mnt/boot
+}
 
-# Install base systemm
-pacstrap /mnt base base-devel linux linux-firmware btrfs-progs intel-ucode intel-media-driver
+base_system() {
+  # Install base systemm
+  pacstrap /mnt base base-devel linux linux-firmware btrfs-progs intel-ucode intel-media-driver
 
-# Setup /etc/fstab
-genfstab -U /mnt >> /mnt/etc/fstab
+  # Setup /etc/fstab
+  genfstab -U /mnt >> /mnt/etc/fstab
 
-# Chroot into new installed system
-arch-chroot /mnt
+  # Chroot into new installed system
+  arch-chroot /mnt
 
-# Install firmwares
-${PACMAN} linux-firmware-bnx2x linux-firmware-liquidio linux-firmware-mellanox \
+  # Install firmwares
+  ${PACMAN} linux-firmware-bnx2x linux-firmware-liquidio linux-firmware-mellanox \
     linux-firmware-nfp linux-firmware-qlogic linux-firmware-whence aic94xx-firmware \
     mkinitcpio-firmware sof-firmware wd719x-firmware                   
 
-# Install some base tools
-${PACMAN} grub nano sudo dhcpcd greetd iwd os-prober ntfs-3g git wget
+  # Install some base tools
+  ${PACMAN} grub nano sudo dhcpcd greetd iwd os-prober ntfs-3g git wget curl
 
-# zsh
-${PACMAN} zsh zsh-completions powerline zsh-autosuggestions \
+  # zsh
+  ${PACMAN} zsh zsh-completions powerline zsh-autosuggestions \
     zsh-syntax-highlighting zsh-powerlevel10k lsd
+}
 
-# Generate locales
-sed 's/#en_US.UTF8 UTF-8/en_US.UTF8 UTF-8/g' /etc/locale.gen
-sed 's/#ca_ES.UTF8 UTF-8/ca_ES.UTF8 UTF-8/g' /etc/locale.gen
+locales() {
+  # Generate locales
+  sed 's/#en_US.UTF8 UTF-8/en_US.UTF8 UTF-8/g' /etc/locale.gen
+  sed 's/#ca_ES.UTF8 UTF-8/ca_ES.UTF8 UTF-8/g' /etc/locale.gen
 
-locale-gen
-localectl set-locale ca_ES.UTF8
+  locale-gen
+  localectl set-locale ca_ES.UTF8
 
-ln -sf /usr/share/zoneinfo/Europe/Rome /etc/localtime
-hwclock --systohc
+  ln -sf /usr/share/zoneinfo/Europe/Rome /etc/localtime
+  hwclock --systohc
+}
 
-# Install bootloader
-grub-install ${INSTALL_DEV}
-grub-mkconfig -o /boot/grub/grub.cfg
+bootloader() {
+  # Install bootloader
+  grub-install ${INSTALL_DEV}
+  grub-mkconfig -o /boot/grub/grub.cfg
+}
 
-# Users
-passwd
-useradd -m -g users -G wheel -s /bin/zsh ${USERNAME}
-passwd ${USERNAME}
+users() {
+  # Users
+  echo "Set root password:"
+  passwd
+  echo "Set password for ${USERNAME}:"
+  useradd -m -g users -G wheel -s /bin/zsh ${USERNAME}
+  passwd ${USERNAME}
 
-export EDITOR="nano"
+  # Add group wheel to sudoers
+  sed 's/# %wheel/%wheel/g' /etc/sudoers
+}
 
-# Add group wheel to sudoers
-sed 's/# %wheel/%wheel/g' /etc/sudoers
+enable_services() {
+  systemctl enable dhcpcd
+  systemctl enable greetd
+}
 
-systemctl enable dhcpcd
-systemctl enable greetd
+wifi() {
+  # iwd (wifi)
+  echo "[Security]
+  EAP-Method=PEAP
+  EAP-Identity=${EAP_IDENTITY}
+  EAP-PEAP-Phase2-Method=MSCHAPV2
+  EAP-PEAP-Phase2-Identity=${EAP_IDENTITY}
+  EAP-PEAP-Phase2-Password=${EAP_PASSWORD}
+  [Settings]
+  AutoConnect=true" > /var/lib/iwd/gencat_ENS_EDU.8021x
+}
 
-# iwd (wifi)
-echo "[Security]
-EAP-Method=PEAP
-EAP-Identity=${EAP_IDENTITY}
-EAP-PEAP-Phase2-Method=MSCHAPV2
-EAP-PEAP-Phase2-Identity=${EAP_IDENTITY}
-EAP-PEAP-Phase2-Password=${EAP_PASSWORD}
-[Settings]
-AutoConnect=true" > /var/lib/iwd/gencat_ENS_EDU.8021x
+yay() {
+  aur yay
+}
 
-# yay
-git clone https://aur.archlinux.org/yay.git
-cd yay
-makepkg -s
-${INSTALL_ZST} *.zst
-cd ..
+session() {
+  # greetd
+  ${PACMAN} greetd
 
-# greetd
-${PACMAN} greetd
+  echo '[terminal]
+  vt = 1
 
-echo '[terminal]
-vt = 1
+  [default_session]
+  command = "tuigreet --cmd hypr"
+  user = "greeter"' >> /etc/greetd/config.toml
 
-[default_session]
-command = "tuigreet --cmd hypr"
-user = "greeter"' >> /etc/greetd/config.toml
+  aur greetd-tuigreet-bin
+}
 
-# greetd tuigreet
-git clone https://aur.archlinux.org/greetd-tuigreet-bin.git
-cd greetd-tuigreet-bin
-makepkg -s
-${INSTALL_ZST} *.zst
-cd ..
-
-# hyperland
-${PACMAN} base-devel gdb ninja gcc cmake libxcb xcb-proto xcb-util \
+hyprland() {
+  ${PACMAN} base-devel gdb ninja gcc cmake libxcb xcb-proto xcb-util \
     xcb-util-keysyms libxfixes libx11 libxcomposite xorg-xinput libxrender \
     pixman wayland-protocols cairo pango seatd libxkbcommon xcb-util-wm \
     xorg-xwayland cmake wlroots mesa git meson polkit lxqt-policykit wofi \
     waybar jq wayland-utils wdisplays
 
-# Install hyprland from Aur
-git clone https://aur.archlinux.org/hyprland-bin.git
-cd hyprland-bin
-makepkg -s
-${INSTALL_ZST} *.zst
-cd ..
+  aur hyprland-bin
 
-# Install Hyprpaper
-git clone https://github.com/hyprwm/hyprpaper  
-cd hyprpaper  
-make all  
-sudo cp build/hyprpaper /usr/local/bin 
-cd ..
+  # Install Hyprpaper
+  git clone https://github.com/hyprwm/hyprpaper  
+  cd hyprpaper  
+  make all  
+  sudo cp build/hyprpaper /usr/local/bin 
+  cd ..
 
-# Copy hyprland conf
-mkdir -p ${CONF_DIR}/hypr
-cp -v hypr/* ${CONF_DIR}/hypr
+  # Copy hyprland conf
+  mkdir -p ${CONF_DIR}/hypr
+  cp -Rv hypr/* ${CONF_DIR}/hypr
 
-# Misc stuff (personal choices)
-${PACMAN} kitty zathura neofetch openssh p7zip pavucontrol
+  ${PACMAN} kitty zathura openssh p7zip
 
-# themeing
-${PACMAN} pop-icon-theme capitaine-cursors breeze-icons
+  ${PACMAN} pop-icon-theme capitaine-cursors breeze-icons
 
-git clone https://aur.archlinux.org/fantome-gtk.git
-cd fantome-gtk
-makepkg -s
-${ZST} 
+  aur fantome-gtk
 
-gsettings set org.gnome.desktop.interface icon-theme Pop  
-#gsettings set org.gnome.desktop.interface gtk-theme Fantome
-gsettings set org.gnome.desktop.interface cursor-theme capitaine-cursors
+  aur nitch
 
-# Full QT libraries
-${PACMAN} qt5 qt6
+  gsettings set org.gnome.desktop.interface icon-theme Pop  
+  #gsettings set org.gnome.desktop.interface gtk-theme Fantome
+  gsettings set org.gnome.desktop.interface cursor-theme capitaine-cursors
 
-# alsa (audio)
-${PACMAN} alsa-lib alsa-plugins alsa-tools alsa-utils alsa-firmware
-gpasswd -a ${USERNAME} audio
+  ${PACMAN} qt5-wayland
+}
 
-# pipewire (audio)
-${PACMAN} pipewire pipewire-audio pipewire-alsa pipewire-pulse pipewire-jack
+audio() {
+  # alsa (audio)
+  ${PACMAN} alsa-lib alsa-plugins alsa-tools alsa-utils alsa-firmware
+  gpasswd -a ${USERNAME} audio
 
-# openal
-${PACMAN} openal realtime-privileges
+  # pipewire (audio)
+  ${PACMAN} pipewire pipewire-audio pipewire-alsa pipewire-pulse pipewire-jack
 
-# fonts
-${PACMAN} adobe-source-code-pro-fonts awesome-terminal-fonts cantarell-fonts \
+  # openal
+  ${PACMAN} openal realtime-privileges
+
+  # Multimedia graph framework
+  ${PACMAN} gst-plugin-pipewire gst-plugins-base gst-plugins-bad gst-plugins-good \
+    gst-plugins-ugly
+
+  ${PACMAN} pavucontrol
+}
+
+fonts() {
+  ${PACMAN} adobe-source-code-pro-fonts awesome-terminal-fonts cantarell-fonts \
     gnu-free-fonts noto-fonts noto-fonts-cjk noto-fonts-emoji noto-fonts-extra \
     siji-ttf ttf-dejavu ttf-font-awesome ttf-font-icons ttf-ionicons \
     ttf-liberation ttf-ms-win10-auto ttf-opensans ttf-sourcecodepro-nerd \
     ttf-unicons ttf-hack powerline-fonts ttc-iosevka
 
-# Gimp (devel)
-#gimp-devel
+  cp -v Madness.ttf /usr/share/fonts/TTF
+}
 
-# Github Desktop
-# github-desktop-bin
+apps() {
+  aur gimp-devel
+  aur github-desktop-bin
+  aur google-chrome
 
-# Google Chrome
-# google-chrome
+  ${PACMAN} grim ffmpegthumbnailer gnome-tweaks jre-openjdk lxqt-policykit \
+    mesa-utils vulkan-intel mlocate moc mpv mtools nfs-utils \
+    youtube-dl zathura-pdf-poppler zathura imv
 
-# Software packages
-${PACMAN} grim feh ffmpegthumbnailer file-roller gnome-tweaks jre-openjdk lxqt-policykit \
-    mesa-utils vulkan-intel mlocate moc mpv mtools neofetch nfs-utils nitrogen \
-    youtube-dl zathura-pdf-poppler zathura zsh zsh-completions zsh-autosuggestions \
-    zsh-syntax-highlighting zsh-theme-powerlevel10k imv  nitch
+  ${PACMAN} zsh zsh-completions zsh-autosuggestions zsh-syntax-highlighting \
+    zsh-theme-powerlevel10k
 
-# Multimedia graph framework
-${PACMAN} gst-plugin-pipewire gst-plugins-base gst-plugins-bad gst-plugins-good \
-    gst-plugins-ugly
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+}
 
-# Thunar
-#${PACMAN} thunar thunar-archive-plugin thunar-volman tumbler
-
-# QT apps
-#${PACMAN} gwenview okular dolphin
-
-# Wine
+wine() {
 ${PACMAN} wine-staging winetricks
-#${PACMAN} lutris MangoHud MangoHud-32bit wine winetricks wine-32bit mesa-dri-32bit \
-#    libGL-32bit amdvlk amdvlk-32bit vkd3d vkd3d-devel vkd3d-32bit vulkan-loader \
-#    vulkan-loader-32bit libX11-devel libX11-devel-32bit libgpg-error libgpg-error-32bit \
-#    gdk-pixbuf gdk-pixbuf-32bit libgcc libgcc-32bit libglvnd libglvnd-32bit 
+}
 
-# Autofs
-${PACMAN} autofs
-systemctl enable autofs
-cp autofs/auto.master.d/programari.autofs /etc/autofs/auto.master.d
-echo "programari -fstype=cifs,username=${PROGRAMARI_USERNAME},password=${PROGRAMARI_PASSWORD} ://192.168.0.151/programari" > /etc/autofs/auto.programari
-echo "operatius -fstype=cifs,username=${PROGRAMARI_USERNAME},password=${PROGRAMARI_PASSWORD} ://192.168.0.151/operatius" >> /etc/autofs/auto.programari
+autofs() {
+  ${PACMAN} autofs
+  systemctl enable autofs
+  cp autofs/auto.master.d/programari.autofs /etc/autofs/auto.master.d
+  echo "programari -fstype=cifs,username=${PROGRAMARI_USERNAME},password=${PROGRAMARI_PASSWORD} ://192.168.0.151/programari" > /etc/autofs/auto.programari
+  echo "operatius -fstype=cifs,username=${PROGRAMARI_USERNAME},password=${PROGRAMARI_PASSWORD} ://192.168.0.151/operatius" >> /etc/autofs/auto.programari
+}
 
-# /usr/local/bin scripts
-cp -v localbin/* /usr/local/bin
+scripts() {
+  cp -v scripts/* /usr/local/bin
+}
+
+# ####################################################################################
+
+set_vars()
+
+#prepare_disk()
+
+base_system()
+locales()
+bootloader()
+users()
+enable_services()
+wifi()
+yay()
+session()
+hyprland()
+audio()
+fonts()
+apps()
+wine()
+autofs()
+scripts()
 
