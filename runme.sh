3b67f64635a29000422e2ab9715a5b2db89d00b2
Gustau Castells <gustaucastells@gmail.com>
2023-05-17 13:08:43 +0200

    Be able to change destination

--- runme.sh
+++ runme.sh
@@ -4,4 +4,11 @@ pacman -Sy --noconfirm wget
 wget https://github.com/karasu/thinkbook/archive/refs/heads/main.tar.gz
 tar xvf main.tar.gz
 cd thinkbook-main
+
+if [ "$1" == "sda" ]; then
+  sed -i -e 's/nvme0n1/sda/g' configuration.json
+  sed -i -e 's/nvme0n1/sda/g' disk_layout.json
+fi
+
 ./install.sh
+
