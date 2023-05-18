05b20fa457a942c92d9ec8aeba1cc4d4c99efc2a
Gustau Castells <gustaucastells@gmail.com>
2023-05-18 20:11:04 +0200

    Working on packages. Added chaotic repo

---
+++ chaotic.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+pacman-key --recv-key FBA220DFC880C036 --keyserver keyserver.ubuntu.com
+pacman-key --lsign-key FBA220DFC880C036
+pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
+
+echo "\n[chaotic-aur]\nInclude = /etc/pacman.d/chaotic-mirrorlist\n" >> /etc/pacman.conf
+
+
