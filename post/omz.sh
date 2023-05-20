616a78fdf34dc2f34001fe33651cb2d04394e899
Gustau Castells <gustaucastells@gmail.com>
2023-05-20 18:50:04 +0200

    Testing omz

--- post/omz.sh
+++ post/omz.sh
@@ -5,8 +5,11 @@ pacman -Sy --noconfirm --needed zsh
 
 echo "Installing OH MY ZSH..."
 
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
 USR=`id -un 1000`
 chsh -s /bin/zsh $USR
 chsh -s /bin/zsh root
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+su -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" $USR
+
+
