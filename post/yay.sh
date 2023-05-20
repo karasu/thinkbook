dc7b8a0d420d5bed4917707618162c462655a7cd
Gustau Castells <gustaucastells@gmail.com>
2023-05-20 18:40:40 +0200

    Modified yay.sh

--- post/yay.sh
+++ post/yay.sh
@@ -4,9 +4,9 @@ YAY="/thinkbook/yay"
 USR=`id -un 1000`
 
 echo "Installing yay using $USR user..."
-
+pacman -Sy --needed --noconfirm go
 git clone https://aur.archlinux.org/yay.git $YAY
 chown -R $USR:$USR $YAY
 cd $YAY
-su -c 'makepkg -sf --needed --noconfirm' $USR
+su -c 'makepkg -f --needed --noconfirm' $USR
 pacman -U --needed --noconfirm yay-*-x86_64.pkg.tar.zst
