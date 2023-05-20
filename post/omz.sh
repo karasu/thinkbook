8a90c63f0c4774bbd56f5bcb90d6b4de64aff29c
Gustau Castells <gustaucastells@gmail.com>
2023-05-20 19:05:14 +0200

    Fixed omz installation

--- post/omz.sh
+++ post/omz.sh
@@ -9,7 +9,13 @@ USR=`id -un 1000`
 chsh -s /bin/zsh $USR
 chsh -s /bin/zsh root
 
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-su -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" $USR
+wget -O /tmp/omzsh-install.sh https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh
+chmod +x /tmp/omzsh-install.sh
+
+# Install for root
+/tmp/omzsh-install.sh
+
+# Install for user 1000
+sudo -u $USR /tmp/omzsh-install.sh
 
 
