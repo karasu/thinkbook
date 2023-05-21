91f821f5e01ef0166dc6c19b156deeb680177012
Gustau Castells <gustaucastells@gmail.com>
2023-05-21 17:51:34 +0200

    Just run from /thinkbook

--- runme.sh
+++ runme.sh
@@ -3,11 +3,11 @@
 pacman-key -u
 pacman-key --populate
 pacman -Sy --noconfirm archlinux-keyring
-
 pacman -S --noconfirm --needed wget
-wget https://github.com/karasu/thinkbook/archive/refs/heads/main.tar.gz
-tar xvf main.tar.gz
-cd thinkbook-main
+
+#wget https://github.com/karasu/thinkbook/archive/refs/heads/main.tar.gz
+#tar xvf main.tar.gz
+#cd thinkbook-main
 
 if [ "$1" == "sda" ]; then
   sed -i -e 's/nvme0n1/sda/g' archinstall/configuration.json
