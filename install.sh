3cdeff0e5b2573f8cb45d658bb43c1e3c525e1b9
Gustau Castells <gustaucastells@gmail.com>
2023-06-05 01:45:56 +0200

    Add link to profile

--- install.sh
+++ install.sh
@@ -2,4 +2,5 @@
 
 loadkeys es
 pacman -S --noconfirm --needed python archinstall
+ln -s /thinkbook/archinstall/profiles ~/.config/archinstall/profiles
 python -m archinstall --config /thinkbook/archinstall/configuration.json --creds /thinkbook/archinstall/credentials.json --disk_layout /thinkbook/archinstall/disk_layout.json
