205b5b41c0fc3d78e929eeecf8c716b26c9c77b4
Gustau Castells <gustaucastells@gmail.com>
2023-06-05 01:47:01 +0200

    Make root config folder

--- install.sh
+++ install.sh
@@ -2,5 +2,6 @@
 
 loadkeys es
 pacman -S --noconfirm --needed python archinstall
+mkdir -p ~/.config/archinstall
 ln -s /thinkbook/archinstall/profiles ~/.config/archinstall/profiles
 python -m archinstall --config /thinkbook/archinstall/configuration.json --creds /thinkbook/archinstall/credentials.json --disk_layout /thinkbook/archinstall/disk_layout.json
