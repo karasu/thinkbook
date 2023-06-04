32a4e7cdf5c8393a200080d007ee173eea0c59ec
Gustau Castells <gustaucastells@gmail.com>
2023-06-05 01:38:33 +0200

    rollback

--- install.sh
+++ install.sh
@@ -1,10 +1,5 @@
 #!/bin/sh
 
 loadkeys es
-pacman -S --noconfirm --needed python python-pip git
-pip uninstall archinstall
-git clone https://github.com/archlinux/archinstall /archinstall
-cd /archinstall
-pip install
-cd /thinkbook
+pacman -S --noconfirm --needed python archinstall
 python -m archinstall --config /thinkbook/archinstall/configuration.json --creds /thinkbook/archinstall/credentials.json --disk_layout /thinkbook/archinstall/disk_layout.json
