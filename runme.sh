05c67a9c52cb0dacb5700fffd0d8fed34349c275
Gustau Castells <gustaucastells@gmail.com>
2023-06-06 19:11:18 +0200

    Testing

--- runme.sh
+++ runme.sh
@@ -2,10 +2,11 @@
 
 # TESTING ONLY
 #########################################################################
-cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.backup
-echo "Server = http://10.1.1.166:8000\n" > /etc/pacman.d/mirrorlist
-cat /etc/pacman.d/mirrorlist.backup >> /etc/pacman.d/mirrorlist
-rm /etc/pacman.d/mirrorlist.backup
+if [ ! -f "/etc/pacman.d/mirrorlist.backup" ]; then
+    cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.backup
+    echo "Server = http://10.1.1.166:8000\n" > /etc/pacman.d/mirrorlist
+    cat /etc/pacman.d/mirrorlist.backup >> /etc/pacman.d/mirrorlist
+fi
 #########################################################################
 
 pacman-key -u
